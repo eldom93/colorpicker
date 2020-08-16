588111cf1436e82a25e8860853abbf9b19e4a9a1
eldom93 <eldomiatyn@yahoo.com>
2020-08-16 18:07:14 -0500

    add:selected color functionality

--- src/components/colorPicker/index.js
+++ src/components/colorPicker/index.js
@@ -3,21 +3,32 @@ import "./index.css";
 const classNames = require('classnames');
 
 export default class ColorPicker extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      sc: "black"
+    }
+  }
+  changeSelectedColor(e){
+    this.setState({
+      sc: e.target.style.backgroundColor
+    })
+  }
   render() {
     let selectedColor;
     return (
       <div>
         <div className="layout-row justify-content-center">
           <div className="card mt-75">
-            <div className="canvas" data-testid="selectedColor">
+            <div style={{'backgroundColor':this.state.sc}} className="canvas" data-testid="selectedColor">
               <p className="text-center mx-auto px-5">{selectedColor}</p>
             </div>
             <div className="card-actions">
               <div className="layout-row justify-content-center align-items-center" data-testid="colorPickerOptions">
                 {this.props.colorPickerOptions.map((color, index) => {
                   return (
-                    <div
+                    <div onClick={(e) => this.changeSelectedColor(e)}
+                     style={{'backgroundColor':color}}
                       className={
                         classNames({
                           'color-box': true,
